c12f8ca628b1bcb03a83c5f25750488fe69b98b6
TonyMonntana <67508413+TonyMonntana@users.noreply.github.com>
2020-08-04 21:02:34 +0600

    Add files via upload

---
+++ Animal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW11
+{
+    abstract class Animal
+    {
+        public abstract int Age { get; set; }
+        public abstract int Weight { get; set; }
+        public abstract string Feed { get; set; }
+
+        public Animal(string name, int age, string gender, int weight)
+
+        public Animal(string name, int age, string gender, int weight, string feed)
+        {
+            Name = name;
+            Age = age;
+            Gender = gender;
+            Weight = weight;
+            Feed = feed;
+        }
+
+        public abstract void Eat();
+        public abstract void WhatFeed();
+        public abstract void Eat(Animal animal);
+  }
+}
